b8189ee92c3718a8a94d8442ff5e7d0949c6214c
SekharRoot <73870058+SekharRoot@users.noreply.github.com>
2026-07-01 18:19:26 +0000

    Deploying to main from @ SekharRoot/Class-teacher@c42165f3ba3d0f89f00081ded309215c262a8ad9 🚀

--- src/api/index.ts
+++
@@ -1,6 +0,0 @@
-export { classesApi } from "./classes";
-export { studentsApi } from "./students";
-export { attendanceApi } from "./attendance";
-export { usersApi } from "./users";
-export { leavesApi } from "./leaves";
-export type { AttendanceRecordSummary } from "./attendance";
