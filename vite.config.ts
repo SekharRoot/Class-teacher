5b0a704995d8b0128d20f74659fbb2d991423ce8
Sekhar <73870058+SekharRoot@users.noreply.github.com>
2026-07-13 07:43:51 +0530

    Fff

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
-import { VitePWA } from "vite-plugin-pwa";
+// import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig(() => {
   const isGitHubActions = process.env.GITHUB_ACTIONS === "true";
@@ -14,32 +14,32 @@ export default defineConfig(() => {
     plugins: [
       react(), 
       tailwindcss(),
-      VitePWA({
-        registerType: "autoUpdate",
-        manifest: {
-          name: "Classroom Manager by Sekhar",
-          short_name: "Classroom",
-          description: "Attendance and Classroom Management System",
-          theme_color: "#ffffff",
-          background_color: "#ffffff",
-          display: "standalone",
-          start_url: base,
-          scope: base,
-          icons: [
-            {
-              src: "https://cdn-icons-png.flaticon.com/512/3587/3587091.png",
-              sizes: "512x512",
-              type: "image/png",
-              purpose: "any maskable"
-            },
-          ],
-        },
-        workbox: {
-          cleanupOutdatedCaches: true,
-          globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
-          maximumFileSizeToCacheInBytes: 10000000,
-        },
-      })
+      // VitePWA({
+      //   registerType: "autoUpdate",
+      //   manifest: {
+      //     name: "Classroom Manager by Sekhar",
+      //     short_name: "Classroom",
+      //     description: "Attendance and Classroom Management System",
+      //     theme_color: "#ffffff",
+      //     background_color: "#ffffff",
+      //     display: "standalone",
+      //     start_url: base,
+      //     scope: base,
+      //     icons: [
+      //       {
+      //         src: "https://cdn-icons-png.flaticon.com/512/3587/3587091.png",
+      //         sizes: "512x512",
+      //         type: "image/png",
+      //         purpose: "any maskable"
+      //       },
+      //     ],
+      //   },
+      //   workbox: {
+      //     cleanupOutdatedCaches: true,
+      //     globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      //     maximumFileSizeToCacheInBytes: 10000000,
+      //   },
+      // })
     ],
     build: {
       outDir: "dist",
