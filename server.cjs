b0f2f0dbcaeee4913947c097746389551c782ccd
SekharRoot <73870058+SekharRoot@users.noreply.github.com>
2026-08-09 18:54:59 +0000

    Deploying to gh-pages from @ SekharRoot/Class-teacher@f7542f440cda3715b515a5e8d59e19eda3de64ea 🚀

--- server.cjs
+++ server.cjs
@@ -34,7 +34,7 @@ process.on("uncaughtException", (err) => {
 });
 async function startServer() {
   const app = (0, import_express.default)();
-  const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3e3;
+  const PORT = Number(process.env.PORT || 3e3);
   const isProduction = process.env.NODE_ENV === "production" || !!process.env.K_SERVICE;
   console.log(
     `Starting server in ${isProduction ? "production" : "development"} mode on port ${PORT}...`
